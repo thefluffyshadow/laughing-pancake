ff807a89bb4116e47e6807aa5b865b5f4afa71e5
Zachary Champion <zchampion270@gmail.com>
2017-08-06 18:00:20 -0600

    messed around with .helloworld

--- src/helloworld.clj
+++ src/helloworld.clj
@@ -1,15 +1,11 @@
 (ns .helloworld)
 ; The traditional "Hello World" Program.
 
+(def hello_name "Kon'Ken")
+
 (defn helloworld
   ([] "Hello World")
-  ([name] (str "Hello " name)))
+  ([you] (str "Hello " you)))
 
-
-(->
-  (println)
-  (helloworld)
-  (str "Zac"))
-
-
-(println (helloworld "Kon'Ken"))
+(println (helloworld))
+(println (helloworld hello_name))
