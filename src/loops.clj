e3638c3aeced0a1e4fbea40804157fc78fafec87
Zachary Champion <zchampion270@gmail.com>
2017-08-06 18:24:37 -0600

    added the doseq statement to the loops example namespace

--- src/loops.clj
+++ src/loops.clj
@@ -1,13 +1,28 @@
 (ns .loops
   (:gen-class))
 
-;; This program displays Hello World
+;; Example of the while loop
 (defn Example []
+  (println "While loop start:")
+
   (def x (atom 1))
 
   (while ( < @x 5)
     (do
       (println @x)
-      (swap! x inc))))
+      (swap! x inc)
+      )
+    )
+  )
+
+;; Example of the doseq loop statement
+(defn Example2 []
+  (println "Doseq loop start")
+  (doseq [n [0 1 2]]
+    (println n)
+    )
+  )
+
 
 (Example)
+(Example2)
