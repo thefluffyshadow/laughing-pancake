e907a149ecf88056d735d6ab9c16f5d299f78708
Zachary Champion <zchampion270@gmail.com>
2017-08-06 18:42:54 -0600

    Added dotimes and loop loop statements to the loop example file.

--- src/loops.clj
+++ src/loops.clj
@@ -1,28 +1,50 @@
 (ns .loops
   (:gen-class))
 
-;; Example of the while loop
+(def condition_end 5)
+
+; Example of the while loop
+; Also includes the mysterious atom data type!
 (defn Example []
-  (println "While loop start:")
+  (println "While loop start")
 
   (def x (atom 1))
 
-  (while ( < @x 5)
+  (while ( < @x condition_end)
     (do
       (println @x)
-      (swap! x inc)
-      )
-    )
-  )
+      (swap! x inc))))
 
 ;; Example of the doseq loop statement
 (defn Example2 []
   (println "Doseq loop start")
-  (doseq [n [0 1 2]]
+  (doseq [n (range condition_end)]
     (println n)
+    )
+  )
+
+;; Example of the dotimes loop statement
+(defn Example3 []
+  (println "Dotimes loop start")
+  (dotimes [n condition_end]
+    (println n)
+    )
+  )
+
+; Example of the loop loop statement
+(defn Example4 []
+  (println "Loop loop start")
+
+  (loop [x 0]
+    (when (< x condition_end)
+      (println x)
+      (recur (inc x))
+      )
     )
   )
 
 
 (Example)
 (Example2)
+(Example3)
+(Example4)
