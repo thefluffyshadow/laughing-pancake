889f478bf96a75c9bfea5a3937078703308ea864
Zachary Champion <zchampion270@gmail.com>
2017-08-11 00:23:18 -0600

    euler001 works! The problem was that it needed to be adding the number twice to the sum if it was a factor of both 3 and 5.

--- src/euler001.clj
+++ src/euler001.clj
@@ -4,24 +4,21 @@
 ;                     in Clojure.
 ;                     The function of this piece of code is to compute the sum of the multiples of 3 and 5 that are less
 ;                     than a certain number - 1000 is the default.
-; Date last updated:  9 August 2017
+; Date last updated:  10 August 2017
 (ns .euler001)
 
 (def total (atom 0))
 (def i (atom 0))
+(def target 1000)
 
-(defn multiples [target]
+(while (< @i target)
+  (cond  ; Test if the current value in i is a mult of 3 or 5.
+    (= (mod @i 3) (mod @i 5) 0) (reset! total (+ @total @i @i))  ; If it's a mult of 3 & 5, add it twice.
+    (= (mod @i 3) 0) (reset! total (+ @total @i))  ; If so, add it to the total.
+    (= (mod @i 5) 0) (reset! total (+ @total @i))  ; If so, add it to the total.
+    )
 
-  (while (< @i target)
-    (cond  ; Test if the current value in i is a mult of 3 or 5.
-      (= (mod @i 3) 0) (reset! total (+ @total @i))  ; If so, add it to the total.
-      (= (mod @i 5) 0) (reset! total (+ @total @i))  ; If so, add it to the total.
-      )
-
-    (swap! i inc)
+  (swap! i inc)
    )
 
-  (println "Sum of the multiples less than" @target ": " @total)
-  )
-
-(multiples 1000)
+(println "Sum of the multiples less than" target ": " @total)
