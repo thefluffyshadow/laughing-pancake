95e4492ed6988854c6ea2c84a05fc3812c898ad9
Zachary Champion <zchampion270@gmail.com>
2017-08-12 15:11:50 -0600

    Fixed the euler001!! Just an issue with how I was trying to pass the arguments. Learning more about how to use the language... ^w^

--- src/CodinGame/the_descent.clj
+++ src/CodinGame/the_descent.clj
@@ -7,7 +7,8 @@
   (while true  ; Game loop
     (loop [i 8]
       (when (> i 0)
-        (let [mountainH (read)
+        (let [mountainH (read)]
+              (swap! mountains assoc (- 8 i) mountainH)
         (recur (dec i)))))
 
     (println (key (apply max-key val @mountains)))))
