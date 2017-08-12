e97f0a74a1d9538d6e981f92614b67bb21f57cce
Zachary Champion <zchampion270@gmail.com>
2017-08-12 14:50:05 -0600

    Cleaned up the_descent. Also, I broke it somehow.

--- src/CodinGame/the_descent.clj
+++ src/CodinGame/the_descent.clj
@@ -1,29 +1,13 @@
 (ns Player
   (:gen-class))
 
-; The while loop represents the game.
-; Each iteration represents a turn of the game
-; where you are given inputs (the heights of the mountains)
-; and where you have to print an output (the index of the mountain to fire on)
-; The inputs you are given are automatically updated according to your last actions.
 (def mountains (atom {}))
 
 (defn -main [& args]
-  (while true
+  (while true  ; Game loop
     (loop [i 8]
       (when (> i 0)
-        (let [mountainH (read)]
-          ; mountainH: represents the height of one mountain.
-          (swap! mountains assoc :i mountainH)  ; TODO: this i seems to associate literally. Why?
-
-
-          ;(binding [*out* *err*]
-          ;         (println (str mountains)))
-          
+        (let [mountainH (read)
         (recur (dec i)))))
-    
-    ; (binding [*out* *err*]
-    ;   (println "Debug messages..."))
-    
-    ; The index of the mountain to fire on.
-    (println (str (key (apply max-key val @mountains))))))
+
+    (println (key (apply max-key val @mountains)))))
