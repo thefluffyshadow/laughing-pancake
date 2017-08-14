85fff80c0d83643b9fd7b15b666d0ccb9217133a
Zachary Champion <zchampion270@gmail.com>
2017-08-13 19:32:32 -0600

    Definitely needed a new frame of thinking. It works!

--- src/CodinGame/power_of_thor_1.clj
+++ src/CodinGame/power_of_thor_1.clj
@@ -1,5 +1,3 @@
-;(ns CodinGame.Power_of_Thor
-;  (:gen-class))
 (ns Player
   (:gen-class))
 
@@ -8,13 +6,23 @@
 (defn -main [& args]
   (let [[lightX lightY initialTX initialTY] (repeatedly 4 read)
         delta-x (- lightX initialTX) delta-y (- lightY initialTY)
+
         dist-x (Math/abs delta-x) dist-y (Math/abs delta-y)
+
         y-seq (cond (neg? delta-y) (repeat dist-y \N)
-                    (pos? delta-y) (repeat dist-y \S))
+                    (pos? delta-y) (repeat dist-y \S)
+                    :else nil)
+
         x-seq (cond (pos? delta-x) (repeat dist-x \E)
-                    (neg? delta-x) (repeat dist-x \W))
+                    (neg? delta-x) (repeat dist-x \W)
+                    :else nil)
+
+        max-length (max dist-x dist-y)
+        normalize-length #(concat % (repeat (- max-length (count %)) ""))
+        moves (map str (normalize-length y-seq) (normalize-length x-seq))
         ]
-    (while true
+
+    (doseq [move moves]
       (let [remainingTurns (read)]
-           (println \E)
+           (println move)
            ))))
