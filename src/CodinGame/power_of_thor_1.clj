5bca37532d4bdeb7af7da1319bffe69a69b7548c
Zachary Champion <zchampion270@gmail.com>
2017-08-13 19:11:26 -0600

    Started over on the thor puzzle. I think I needed a new frame of thinking.

--- src/CodinGame/power_of_thor_1.clj
+++ src/CodinGame/power_of_thor_1.clj
@@ -3,18 +3,18 @@
 (ns Player
   (:gen-class))
 
-(defn -main [& args]
-  (let [lightX (read) lightY (read) initialTX (read) initialTY (read)]
-    (while true
-      (let [remainingTurns (read) dir_str (atom "")
-            dX (- lightX initialTX) dY (- lightY initialTY)]
-        (cond
-          (< dY 0) (swap! dir_str str "N")     ; tell Thor to start going North.
-          (> dY 0) (swap! dir_str str "S")     ; Otherwise, go south.
-          (= dY 0) (swap! dir_str str ""))
-        (cond
-          (< dX 0) (swap! dir_str str "W")   ; tell Thor to go westerly.
-          (> dX 0) (swap! dir_str str "E")   ; Otherwise, tell Thor to go easterly.
-          (= dX 0) (swap! dir_str str ""))
+; Referenced https://gist.github.com/MikeMKH/730228a3f658b9b76b72
 
-        (println @dir_str)))))
+(defn -main [& args]
+  (let [[lightX lightY initialTX initialTY] (repeatedly 4 read)
+        delta-x (- lightX initialTX) delta-y (- lightY initialTY)
+        dist-x (Math/abs delta-x) dist-y (Math/abs delta-y)
+        y-seq (cond (neg? delta-y) (repeat dist-y \N)
+                    (pos? delta-y) (repeat dist-y \S))
+        x-seq (cond (pos? delta-x) (repeat dist-x \E)
+                    (neg? delta-x) (repeat dist-x \W))
+        ]
+    (while true
+      (let [remainingTurns (read)]
+           (println \E)
+           ))))
