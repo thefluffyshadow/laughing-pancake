a461fda0297e6d3eb83847a67038f0ad18b84459
Zachary Champion <zchampion270@gmail.com>
2017-08-19 14:35:37 -0600

    Minor changes and a date correction update.

--- src/project_euler/euler002.clj
+++ src/project_euler/euler002.clj
@@ -9,7 +9,7 @@
 ; By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the
 ; even-valued terms.
 
-; Last Updated 18 August 2017
+; Last Updated 19 August 2017
 
 (defn DoTheThing [limit]
   (loop [n1 1, n2 2, accumulator 2]
@@ -18,9 +18,10 @@
         accumulator  ; we're done
         (recur n2 f (if (= (mod f 2) 0) (+ accumulator f) accumulator))))))
 
+(def limit 4000000)
 (def correct-answer 461732)
 
 (defn -main [& args]
-  (let [answer (DoTheThing 4000000)]
+  (let [answer (DoTheThing limit)]
     (println "Sum of the even fibonacci terms under 4,000,000 =" answer "which is"
              (if (= answer correct-answer) "correct!" (str (- answer correct-answer) " off.")))))
